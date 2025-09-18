fd855cc35e0603aeb6f925b3f4426f9b3ee21f2e
Jenna Deamer <jennadeamer@gmail.com>
2025-09-17 21:07:00 -0400

    Add gameboard cell indication for ships, hits and misses

--- src/domController.js
+++ src/domController.js
@@ -1,18 +1,69 @@
 function domController(humanBoardInstance, computerBoardInstance) {
 	const humanBoard = document.querySelector('#human-board');
-	const computerBoard = document.querySelector('#computer-board');
+	const humanShips = humanBoardInstance.ships;
 
+	const computerBoard = document.querySelector('#computer-board');
+	const computerShips = computerBoardInstance.ships;
+	const computerMissedSquares = computerBoardInstance.missedSquares;
+
+    computerMissedSquares.push([0,0])
+    const computerHitSquares = computerBoardInstance.hitSquares;
+    computerHitSquares.push([1,5]);
+
+	function highlightHumanShips(col, row, cell) {
+		const isShipCell = humanShips.some((ship) =>
+			ship.positions.some((position) => {
+				const [shipCol, shipRow] = position;
+				return shipCol === col && shipRow === row; // returns true is pos matches
+			})
+		);
+
+		if (isShipCell) {
+			cell.style.backgroundColor = '#101e47';
+		}
+	}
+
+	function highlightMissedSquares(col, row, cell, boardInstance) {
+        const missedSquares = boardInstance.missedSquares;
+
+		const isCellMissed = missedSquares.some(([missedCol, missedRow]) => {
+			return missedCol === col && missedRow === row;
+		});
+
+        if(isCellMissed){
+            cell.style.backgroundColor = '#3e4247'
+        }
+	}
+
+    function highlightHitSquares(col,row,cell, boardInstance){
+        const hitSquares = boardInstance.hitSquares;
+
+        const isCellHit = hitSquares.some(([hitCol,hitRow]) => {
+            return hitCol === col && hitRow === row;
+        })
+
+        if(isCellHit){
+            cell.style.backgroundColor = '#cc2a25'
+        }
+    }
 	function renderBoard(boardElement, boardInstance) {
+        boardElement.innerHTML = ''; // clear board
+
 		for (let col = 0; col < 10; col++) {
 			for (let row = 0; row < 10; row++) {
 				const cell = document.createElement('button');
 
-				// Show ships if human instance
+				cell.dataset.col = col;
+				cell.dataset.row = row;
+
+				// Highlight human player's ships
 				if (boardInstance === humanBoardInstance) {
-                    console.log('Ships:', humanBoardInstance.ships);
+					highlightHumanShips(col, row, cell);
 				}
 
-				// Disable hit and missed squares
+				// Mark hits & misses
+				highlightMissedSquares(col, row, cell, boardInstance);
+                highlightHitSquares(col,row,cell,boardInstance)
 
 				boardElement.appendChild(cell);
 			}
